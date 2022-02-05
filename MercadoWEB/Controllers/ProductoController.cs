8d7a2aaafb7c9b3a993f8ea620011d1f9f5f246b
menocal02 <estefanmenocal@gmail.com>
2022-02-05 15:04:17 -0600

    primeros cambios

---
+++ MercadoWEB/Controllers/ProductoController.cs
@@ -0,0 +1,21 @@
+﻿using MercadoWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MercadoWEB.Controllers
+{
+    public class ProductoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var productos = new List<ProductoModel>() 
+            {
+                new ProductoModel(){ NombreProducto = "Arroz", Descripcion="80% grano entero", Categoria="Granos básicos", Precio = 15},
+                new ProductoModel(){ NombreProducto = "Frijoles", Descripcion="grano rojo", Categoria="Granos básicos", Precio = 22},
+                new ProductoModel(){ NombreProducto = "Gaseosa", Descripcion="grano rojo", Categoria="Bebidas", Precio = 30}
+            };
+
+            return View("Productos",productos);
+        }
+    }
+}
