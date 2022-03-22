3d5fb1bc9fb55e2bd4d89ed885feee74b120ff82
menocal02 <estefanmenocal@gmail.com>
2022-03-22 11:11:16 -0600

    -

---
+++ MercadoWEB/wwwroot/lib/troastr/js/toastrOptions.js
@@ -0,0 +1,17 @@
+﻿toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-full-width",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "1000",
+    "hideDuration": "1000",
+    "timeOut": "2500",
+    "extendedTimeOut": "1000",
+    "showEasing": "linear",
+    "hideEasing": "linear",
+    "showMethod": "slideDown",
+    "hideMethod": "slideUp"
+}
